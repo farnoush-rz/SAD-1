980ebf2dfac4ff64786639605d7750ebcbc20cf4
nikooGolpayegani <79171501+nikooGolpayegani@users.noreply.github.com>
2021-02-18 21:42:15 +0330

    Add files via upload

---
+++ BackupController.java
@@ -0,0 +1,22 @@
+
+package javaapplication13;
+
+
+public class BackupController {
+    
+    private int lastSync;
+    private boolean isMaster;
+    private boolean isSyncing;
+    
+    //*******************************************
+    
+    public void BackupController(int lastSync, boolean isMaster, boolean isSyncing){
+        
+        
+    }
+    
+    //*********************************************
+    private void autoSync(){
+        
+    }
+}
